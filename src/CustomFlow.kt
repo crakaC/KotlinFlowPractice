d61fa8abe3f2cf85a572e1e3b1228fe73f6909bc
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2022-03-09 21:56:28 +0900

    zip practice

--- src/CustomFlow.kt
+++ src/CustomFlow.kt
@@ -3,10 +3,10 @@ import kotlinx.coroutines.runBlocking
 
 fun main(): Unit = runBlocking {
     launch {
-        flowOf("First", "Second")
+        myFlowOf("First", "Second")
             .map { "$it modified 1st" }
             .map { "$it modified 2nd" }
-            .collect(object : Collector<String> {
+            .collect(object : MyCollector<String> {
                 override fun emit(value: String) {
                     println(value)
                 }
@@ -14,27 +14,27 @@ fun main(): Unit = runBlocking {
     }
 }
 
-private interface Collector<in T> {
+private interface MyCollector<in T> {
     fun emit(value: T)
 }
 
-private interface Flow<out T> {
-    fun collect(collector: Collector<T>)
+private interface MyFlow<out T> {
+    fun collect(collector: MyCollector<T>)
 }
 
-private fun <T> flowOf(vararg value: T) = object : Flow<T> {
-    override fun collect(collector: Collector<T>) {
+private fun <T> myFlowOf(vararg value: T) = object : MyFlow<T> {
+    override fun collect(collector: MyCollector<T>) {
         for (v in value) {
             collector.emit(v)
         }
     }
 }
 
-private inline fun <T, R> Flow<T>.map(crossinline transform: (value: T) -> R): Flow<R> {
+private inline fun <T, R> MyFlow<T>.map(crossinline transform: (value: T) -> R): MyFlow<R> {
     val upstream = this
-    return object : Flow<R> {
-        override fun collect(collector: Collector<R>) {
-            upstream.collect(object : Collector<T> {
+    return object : MyFlow<R> {
+        override fun collect(collector: MyCollector<R>) {
+            upstream.collect(object : MyCollector<T> {
                 override fun emit(value: T) {
                     val newValue = transform(value)
                     collector.emit(newValue)
